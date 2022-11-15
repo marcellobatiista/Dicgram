7699bffa75fff205301e62910a8c63c9b406f6b2
Marcelo Batista <39688655+marcellobatiista@users.noreply.github.com>
2022-11-15 20:59:58 -0300

    attus

--- exemplos/averso_a_grupo_bot.py
+++ exemplos/averso_a_grupo_bot.py
@@ -9,7 +9,7 @@ bot = Bot(token='<TOKEN>', nome='SuporteBot')
 
 def sair_do_grupo(mim, msg, args):
     try:
-        if msg.message.new_chat_participant.username == 'applicativo_bot':
+        if msg.message.new_chat_participant.username == '<USER_NAME_BOT>':
             mim.sendmessage(chat_id=msg.message.chat.id,
                             text='Não gosto de grupos, então vou sair. Fuiiiz!')
             mim.leavechat(chat_id=msg.message.chat.id)
