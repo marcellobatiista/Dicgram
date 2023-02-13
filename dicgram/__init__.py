35430f01ed39ddbb0f7dfbfb20da03673707ab1c
Marcelo <batista.marcelo34@gmail.com>
2023-02-12 21:42:58 -0300

    nova versao 2.0.7

--- dicgram/__init__.py
+++ dicgram/__init__.py
@@ -1,7 +1,7 @@
 from dicgram.cliente import Bot
 
 copy = 'Copyright (c) 2022 Marcelo <https://github.com/marcellobatiista>'
-version = '2.0.5'
+version = '2.0.7'
 autor = 'Marcelo Batista'
 email = 'batista.marcelo34@gmail.com'
 
